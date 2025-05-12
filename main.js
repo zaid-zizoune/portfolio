a77a96498977d6992341422bb4b422c8484a5712
zaid zizoune <Zaidzizoune@gmail.com>
2025-05-13 00:52:06 +0200

    Final UI adjustments and bug fixes

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
-      const navLinks = document.querySelectorAll('nav a');
+const navLinks = document.querySelectorAll('nav a');
 
-      function setActiveLink() {
-        navLinks.forEach(link => {
-          link.classList.remove('active');
-          if (link.getAttribute('href') === window.location.hash) {
-            link.classList.add('active');
-          }
-        });
-      }
+function setActiveLink() {
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === window.location.hash) {
+      link.classList.add('active');
+    }
+  });
+}
 
-      window.addEventListener('load', setActiveLink);
-      window.addEventListener('hashchange', setActiveLink);
-      window.addEventListener('scroll', () => {
-        const sections = document.querySelectorAll("section");
-        const navLinks = document.querySelectorAll("nav a");
-      
-        let current = "";
-      
-        sections.forEach((section) => {
-          const sectionTop = section.offsetTop;
-          const sectionHeight = section.clientHeight;
-          if (pageYOffset >= sectionTop - sectionHeight / 3) {
-            current = section.getAttribute("id");
-          }
-        });
-      
-        navLinks.forEach((link) => {
-          link.classList.remove("active");
-          if (link.getAttribute("href") === "#" + current) {
-            link.classList.add("active");
-          }
-        });
-      });
+window.addEventListener('load', setActiveLink);
+window.addEventListener('hashchange', setActiveLink);
+window.addEventListener('scroll', () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav a");
+
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
